53639a10cf2368eaceb93dcb4c7367c64a67ef6d
morantg <bernanosg@gmail.com>
2019-05-27 10:05:54 +0200

    ajout de la contrainte d'unicité pour le nom d'une figure

--- src/Entity/Figure.php
+++ src/Entity/Figure.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FigureRepository")
+ * @UniqueEntity("nom")
  */
 class Figure
 {
@@ -19,7 +22,7 @@ class Figure
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $nom;
 
