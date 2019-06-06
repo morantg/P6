5c4e57a12bc1e476b5ffdd2b0c03d4f9bc3736a2
morantg <bernanosg@gmail.com>
2019-06-06 16:21:32 +0200

    ajout de la possibilité de supprimer un media lors de la création d'une figure

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,11 @@ jQuery(document).ready(function() {
     // Get the ul that holds the collection of tags
     $collectionHolder = $('ul.media');
 
+    // add a delete link to all of the existing tag form li elements
+    $collectionHolder.find('li').each(function() {
+        addTagFormDeleteLink($(this));
+    });
+
     // add the "add a tag" anchor and li to the tags ul
     $collectionHolder.append($newLinkLi);
 
@@ -45,4 +50,17 @@ function addTagForm($collectionHolder, $newLinkLi) {
     // Display the form in the page in an li, before the "Add a tag" link li
     var $newFormLi = $('<li></li>').append(newForm);
     $newLinkLi.before($newFormLi);
+
+    // add a delete link to the new form
+    addTagFormDeleteLink($newFormLi);
+}
+
+function addTagFormDeleteLink($tagFormLi) {
+    var $removeFormButton = $('<button type="button" class="btn btn-danger">Delete this media</button>');
+    $tagFormLi.append($removeFormButton);
+
+    $removeFormButton.on('click', function(e) {
+        // remove the li for the tag form
+        $tagFormLi.remove();
+    });
 }
