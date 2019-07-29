bf279ce739ad209995865a9e56b3d022323c0a35
morantg <bernanosg@gmail.com>
2019-07-29 16:38:14 +0200

    réorganisation du projet + ajout section mes figures

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -21,103 +21,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class FigureController extends AbstractController
 {
-    /**
-     * @Route("/figure/new", name="figure_create")
-     */
-    public function create(Request $request, ObjectManager $manager, UserInterface $user)
-    {
-        $figure = new Figure();
-        $form = $this->createForm(FigureType::class, $figure);
-        $form->handleRequest($request);
-        
-        if($form->isSubmitted() && $form->isValid()){
-            $figure->setUser($user);
-            $figure->setAjoutAt(new \Datetime);
-            // upload de l'image à la une
-            /** @var UploadedFile $uploadedFile */
-            $uploadedFile = $form['image_une']->getData();
-            $fileName = md5(uniqid()).'.'.$uploadedFile->guessExtension();
-            $uploadedFile->move(
-                $this->getParameter('upload_directory'),
-                $fileName
-            );
-            $figure->setImageUne($fileName);
-            //upload des media
-            for($i = 0; $i < 3; $i++){
-                $media = new Media();
-                $uploadedMedia = $form['media' . $i]->getData();
-                if($uploadedMedia){
-                    $fileMediaName = md5(uniqid()).'.'.$uploadedMedia->guessExtension();
-                    $uploadedMedia->move(
-                        $this->getParameter('upload_directory'),
-                        $fileMediaName
-                    );
-                    $media->setUrl($fileMediaName);
-                    $media->setFormat('image');
-                    $figure->addMedium($media);
-                }
-            }
-            $manager->persist($figure);
-            $manager->flush();
-            
-            return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
-        }
-        return $this->render('figure/create.html.twig', [
-            'formFigure' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/figure/{id}/edit", name="figure_edit", methods="GET|POST")
-     */
-    public function edit(Figure $figure, Request $request, ObjectManager $manager, UserInterface $user = null)
-    {
-        $this->denyAccessUnlessGranted('ROLE_USER');
-        if($user != $figure->getUser()){
-            //$this->denyAccessUnlessGranted('ROLE_ADMIN');
-            return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
-        }
-        $form = $this->createForm(FigureType::class, $figure);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-            $figure->setModifAt(new \Datetime);
-            // upload de l'image à la une
-            /** @var UploadedFile $uploadedFile */
-            $uploadedFile = $form['image_une']->getData();
-            if($uploadedFile){
-            $fileName = md5(uniqid()).'.'.$uploadedFile->guessExtension();
-            $uploadedFile->move(
-                $this->getParameter('upload_directory'),
-                $fileName
-            );
-            $figure->setImageUne($fileName);
-            }
-            //upload des media
-            $media = $figure->getMedia();
-            foreach($media as $a => $medium){
-                $uploadedMedia = $form['media' . $a]->getData();
-                if($uploadedMedia){
-                    $fileMediaName = md5(uniqid()).'.'.$uploadedMedia->guessExtension();
-
-                    $uploadedMedia->move(
-                        $this->getParameter('upload_directory'),
-                        $fileMediaName
-                    );
-                    $medium->setUrl($fileMediaName);
-                }
-            }
-            $manager->persist($figure);
-            $manager->flush();
-            
-            return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
-        }
-        return $this->render('figure/edit.html.twig', [
-            'formFigure' => $form->createView(),
-            'figure' => $figure,
-        ]);
-    }
-
+  
     /**
      * @Route("/figure/{id}", name="figure_show")
      */
@@ -129,22 +33,5 @@ class FigureController extends AbstractController
         return $this->render('figure/show.html.twig', [
             'figure' => $figure
         ]);
-    }
-
-    /**
-     * @Route("/figure/{id}/edit", name="figure_delete", methods="DELETE")
-     * @param Figure $figure
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function delete(Figure $figure, Request $request, ObjectManager $manager) {
-        if ($this->isCsrfTokenValid('delete' . $figure->getId(), $request->get('_token'))) {
-            $media = $figure->getMedia();
-            foreach($media as $medium){
-            $manager->remove($medium);
-            }
-            $manager->remove($figure);
-            $manager->flush();
-        }
-        return $this->redirectToRoute('home');
     }
 }
